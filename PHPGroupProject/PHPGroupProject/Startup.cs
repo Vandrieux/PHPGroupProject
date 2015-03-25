448611f5f9d2003f20e1eb0e512854e2af099eb9
prabhjot <prabhjot.prabhjot@robertsoncollege.net>
2015-03-24 17:14:36 -0700

    new one

---
+++ PHPGroupProject/PHPGroupProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PHPGroupProject.Startup))]
+namespace PHPGroupProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
